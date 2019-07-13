bf6ce4f7fa6bf015bf557b2043dd03c8f824ff5d
Antti <karvanen.antti@gmail.com>
2019-07-13 11:43:37 +0300

    added image with text

--- index.js
+++ index.js
@@ -21,6 +21,32 @@ app.get('/color.png', (req, res) => {
   })
 })
   
+app.get('/text-image/:text', (req, res) => {
+  const [red, green, blue] = Array(3).fill().map(_ => Math.round(Math.random() * 255))
+  const color = `#${red.toString(16).padStart(2, '0')}${green.toString(16).padStart(2, '0')}${blue.toString(16).padStart(2, '0')}`
+  console.log(color)
+
+  const x = 500
+  const y = 100
+
+  let image = new Jimp(x, y, color, (err, image) => {
+    if (err) throw err
+  })
+
+  const text = req.params.text
+
+  Jimp.loadFont(Jimp.FONT_SANS_64_BLACK)
+    .then(font => {
+      image.print(font, 10, 10, text.slice(0, -4))
+      return image
+    })
+    .then(img => {
+      img.getBuffer(Jimp.AUTO, (e, buffer) => {
+        if (e) throw e
+        res.send(buffer)
+      })
+    })
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
